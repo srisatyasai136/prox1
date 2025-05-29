39c15a69080434310627f6e688369878ab7c22e7
sri satya sai <srisatyasai136@gmail.com>
2025-05-29 22:40:08 +0530

    Add files via upload

--- proxy_project/settings.py
+++ proxy_project/settings.py
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['prox1-1.onrender.com', 'localhost', '127.0.0.1']
