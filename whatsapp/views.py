971851ddcee6456a2f740684f111e7e9d338e2d3
sri satya sai <srisatyasai136@gmail.com>
2025-05-29 22:48:58 +0530

    Add files via upload

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -6,17 +6,17 @@ from django.http import response
 # Temporary message store (use DB in real app)
 latest_message = {"text": "", "timestamp": None}
 
-# @csrf_exempt
-# def receive_message(request):
-#     if request.method == "POST":
-#         message_body = request.POST.get("Body")
-#         if message_body:
-#             latest_message["text"] = message_body
-#             latest_message["timestamp"] = timezone.now()
-#             return JsonResponse({"status": "success"})
-#     return JsonResponse({"status": "error"})
+@csrf_exempt
+def receive_message(request):
+    if request.method == "POST":
+        message_body = request.POST.get("Body")
+        if message_body:
+            latest_message["text"] = message_body
+            latest_message["timestamp"] = timezone.now()
+            return JsonResponse({"status": "success"})
+    return JsonResponse({"status": "error"})
 
-def  receive_message(request):
-    return response("hello world")
+# def  receive_message(request):
+#     return response("hello world")
 def get_latest_message(request):
     return JsonResponse(latest_message)
